4f4d06bebcad48ace037f8e74f8557b156f68103
fengjw <fengjwxx@163.com>
2017-07-20 23:53:31 +0800

    commit

---
+++ FragmentLearn/app/src/main/java/com/example/fengjw/fragmentlearn/activity/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.fengjw.fragmentlearn.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.fengjw.fragmentlearn.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button1:
+                Intent intent = new Intent(MainActivity.this,SimpleFragmentActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ FragmentLearn/app/src/main/java/com/example/fengjw/fragmentlearn/activity/SimpleFragmentActivity.java
@@ -0,0 +1,15 @@
+package com.example.fengjw.fragmentlearn.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fengjw.fragmentlearn.R;
+
+public class SimpleFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_fragment);
+    }
+}
