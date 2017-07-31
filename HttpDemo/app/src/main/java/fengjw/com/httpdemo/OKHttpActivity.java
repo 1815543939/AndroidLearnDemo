a5f1a23b677f0b031ff10ac5895fbc1c1bd682de
fengjw <fengjwxx@163.com>
2017-07-31 22:54:00 +0800

    commit

--- HttpDemo/app/src/main/java/fengjw/com/httpdemo/OKHttpActivity.java
+++ HttpDemo/app/src/main/java/fengjw/com/httpdemo/OKHttpActivity.java
@@ -2,9 +2,14 @@ package fengjw.com.httpdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -36,15 +41,31 @@ public class OKHttpActivity extends AppCompatActivity implements View.OnClickLis
             public void run() {
                 try{
                     OkHttpClient client = new OkHttpClient();
-                    Request request = new Request.Builder().url("http://www.baidu.com").build();
+                    Request request = new Request.Builder().url("http://45.32.253.249/get_data.json").build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
-                    showResponse(responseData);
+                    //showResponse(responseData);
+                    parseJSONWithJSONObject(responseData);
                 }catch (Exception e){
                     e.printStackTrace();
                 }
             }
         }).start();
+    }
+
+    private void parseJSONWithJSONObject(String jsonData){
+        try{
+            JSONArray jsonArray = new JSONArray(jsonData);
+            for (int i = 0; i < jsonArray.length(); i ++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String id = jsonObject.getString("id");
+                Log.d("fengjw",id);
+                String name = jsonObject.getString("name");
+                Log.d("fengjw",name);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     private void showResponse(final String responseData){
