d1fc6caa6773a47039b3f9b8f1cb39c792844781
fengjw <fengjwxx@163.com>
2017-08-14 09:43:05 +0800

    。。

--- ServiceBestPractice/app/src/main/java/fengjw/com/servicebestpractice/DownloadTask.java
+++ ServiceBestPractice/app/src/main/java/fengjw/com/servicebestpractice/DownloadTask.java
@@ -4,9 +4,9 @@ import android.os.AsyncTask;
 import android.os.Environment;
 
 import java.io.File;
-import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -38,23 +38,109 @@ public class DownloadTask extends AsyncTask<String, Integer, Integer> {
     @Override
     protected Integer doInBackground(String... strings) {
         InputStream is = null;
+        RandomAccessFile savedFile = null;
         File file = null;
 
         try {
-            long downloadLength = 0;
+            long downloadLength = 0;//记录已下载的文件长度
             String downloadUrl = strings[0];
             String fileName = downloadUrl.substring(downloadUrl.lastIndexOf("/"));
-            String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+            String directory = Environment.getExternalStoragePublicDirectory
+                    (Environment.DIRECTORY_DOWNLOADS).getPath();
             file = new File(directory + fileName);
             if (file.exists()){
                 downloadLength = file.length();
             }
-            long contentLength = getContentLength();
-        }catch (Exception e){
+            long contentLength = getContentLength(downloadUrl);
+            if (contentLength == 0){
+                return TYPE_FAILED;
+            }else if (contentLength == downloadLength){
+                return TYPE_SUCCESS;
+            }
 
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .addHeader("RANGE", "bytes=" + downloadLength + "-")
+                    .url(downloadUrl)
+                    .build();
+            Response response = client.newCall(request).execute();
+            if (response != null){
+                is = response.body().byteStream();
+                savedFile = new RandomAccessFile(file, "rw");
+                savedFile.seek(downloadLength); //跳过已下载的字节
+                byte[] b = new byte[1024];
+                int total = 0;
+                int len;
+                while ((len = is.read(b)) != -1){
+                    if (isCanceled){
+                        return TYPE_CANCELED;
+                    }else if (isPaused){
+                        return TYPE_PASUED;
+                    }else {
+                        total += len;
+                        savedFile.write(b, 0, len);
+                        //计算已下载的百分比
+                        int progress = (int) ((total + downloadLength) + 100 / contentLength);
+                        publishProgress(progress);
+                    }
+                }
+                response.body().close();
+                return TYPE_SUCCESS;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                if (is != null){
+                    is.close();
+                }
+                if (savedFile != null){
+                    savedFile.close();
+                }
+                if (isCanceled && file != null){
+                    file.delete();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
 
-        return null;
+        return TYPE_FAILED;
+    }
+
+    protected void onProgressUpdate(Integer... values){
+        int progress = values[0];
+        if (progress > lastProgress){
+            mListener.onProgress(progress);
+            lastProgress = progress;
+        }
+    }
+
+    protected void onPostExecute(Integer status){
+        switch (status){
+            case TYPE_CANCELED:
+                mListener.onCanceled();
+                break;
+            case TYPE_SUCCESS:
+                mListener.onSuccess();
+                break;
+            case TYPE_FAILED:
+                mListener.onFailed();
+                break;
+            case TYPE_PASUED:
+                mListener.onPaused();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void pauseDownload(){
+        isPaused = true;
+    }
+
+    public void cancelDownload(){
+        isCanceled = true;
     }
 
     private long getContentLength(String downloadUrl) throws IOException{
--- ServiceBestPractice/app/src/main/java/fengjw/com/servicebestpractice/MainActivity.java
+++ ServiceBestPractice/app/src/main/java/fengjw/com/servicebestpractice/MainActivity.java
@@ -1,13 +1,100 @@
 package fengjw.com.servicebestpractice;
 
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private String TGA = "MainActivity";
+
+    private Button startDownload;
+    private Button pauseDownload;
+    private Button cancelDownload;
+
+    private DownloadService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            downloadBinder = (DownloadService.DownloadBinder) iBinder;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startDownload = (Button) findViewById(R.id.Start_Download);
+        pauseDownload = (Button) findViewById(R.id.Pause_Download);
+        cancelDownload = (Button) findViewById(R.id.Cancel_Download);
+        startDownload.setOnClickListener(this);
+        pauseDownload.setOnClickListener(this);
+        cancelDownload.setOnClickListener(this);
+        //Intent
+        Intent intent = new Intent(this, DownloadService.class);
+        startActivity(intent);
+        bindService(intent, connection, BIND_AUTO_CREATE); //绑定服务
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (downloadBinder == null){
+            return;
+        }
+        switch (view.getId()){
+            case R.id.Start_Download:
+                String url = "https://10.0.2.2/test.zip";
+                downloadBinder.startDownload(url);
+                break;
+            case R.id.Pause_Download:
+                downloadBinder.pauseDownload();
+                break;
+            case R.id.Cancel_Download:
+                downloadBinder.cancelDownload();
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "拒绝权限将无法使用程序", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(connection);
     }
 }
